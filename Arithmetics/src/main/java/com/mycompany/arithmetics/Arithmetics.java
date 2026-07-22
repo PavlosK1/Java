4653cf556eaa8685db5edfd47e2a93247262f291
PavlosK1 <paul.kourtidis1@gmail.com>
2026-07-22 13:13:30 +0300

    Add files via upload

---
+++ Arithmetics/src/main/java/com/mycompany/arithmetics/Arithmetics.java
@@ -0,0 +1,34 @@
+package com.mycompany.arithmetics;
+
+public class Arithmetics {
+
+    public static void main(String[] args) {
+        //expression = operands & operators
+        //operands = values, variables, numbers, quantity
+        //operators = +, -, *, /, %(modulus returns the remainder after dividing one number by another)
+        
+        //int x = 10;
+        //int y = 3;
+        //System.out.println(x+y);
+        
+        //int x = 10;
+        //x--;
+        //System.out.println(x);
+        
+        //int x = 10;
+        //x+=3;
+        //System.out.println(x);
+        
+        //int x = 10;
+        //x = x * 3;
+        //System.out.println(x);
+        
+        //int x = 10;
+        //x = x % 3;
+        //System.out.println(x);
+        
+        double x = 10;
+        x = x / 3;
+        System.out.println(x);
+    }
+}
