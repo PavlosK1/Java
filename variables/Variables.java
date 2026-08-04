73cd1ec30465f0f29966815ace341128f103c657
PavlosK1 <paul.kourtidis1@gmail.com>
2026-08-04 13:25:36 +0300

    Add files via upload

---
+++ variables/Variables.java
@@ -0,0 +1,23 @@
+package com.mycompany.variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        // Declaration = Ορισμος
+        int x =123;
+        long b = 6546465465465L;
+        double a = 3.14;
+        boolean c = true;
+        char symbol = '@';
+        String name = "One";
+        
+        
+        System.out.println(x);
+        System.out.println(b);
+        System.out.println("Pi = "+ a);
+        System.out.println(c);
+        System.out.println("symbol = "+symbol);
+        System.out.println("Lesson = "+name);
+        System.out.println(x+" "+name+" "+c);
+    }
+}
