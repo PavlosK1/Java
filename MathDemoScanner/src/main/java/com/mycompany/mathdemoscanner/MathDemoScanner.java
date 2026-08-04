af8de1ce0e06449e6d3db9f7ca9edc9393d545e3
PavlosK1 <paul.kourtidis1@gmail.com>
2026-08-04 13:19:25 +0300

    Add files via upload

---
+++ MathDemoScanner/src/main/java/com/mycompany/mathdemoscanner/MathDemoScanner.java
@@ -0,0 +1,20 @@
+package com.mycompany.mathdemoscanner;
+
+import java.util.Scanner;
+
+public class MathDemoScanner {
+
+    public static void main(String[] args) {
+        double x, y, z;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter side x: ");
+        x = scanner.nextDouble();
+        System.out.println("Enter side y: ");
+        y = scanner.nextDouble();
+        
+        z = Math.sqrt(((x*x)+(y*y)));
+        System.out.println("The hypotenuse is: "+z);
+        scanner.close();
+    }
+}
